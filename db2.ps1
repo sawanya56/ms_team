aa6e27f45c7cbf7babb921065de4c119868ccaa6
sawanya56 <tongnun74@gmail.com>
2023-06-13 14:37:14 +0700

    prepare event

--- db2.ps1
+++ db2.ps1
@@ -52,6 +52,79 @@ function AddInstructor {
     return $sectionId
 }
 
+function AddEvent {
+    param(
+        [Parameter(Position = 0, Mandatory = $true)]
+        [string] $sectionId,
+        [Parameter(Position = 1, Mandatory = $true)]
+        [string] $groupId,
+        [Parameter(Position = 2, Mandatory = $true)]
+        [string] $startDate,
+        [Parameter(Position = 3, Mandatory = $true)]
+        [string] $endDate
+    )
+    $startDate = $startDate.Replace("00:00:00", "");
+    $arrayStartDate = $startDate.Split("/");
+
+    $endDate = $endDate.Replace("00:00:00", "");
+    $arrayEndDate = $endDate.Split("/");
+
+
+    $startDate = Get-Date -Year $arrayStartDate[2] -Month $arrayStartDate[0] -Day $arrayStartDate[1]
+    $newStartDate = [DateTime]::ParseExact($startDate, "MM/dd/yyyy HH:mm:ss", $null)
+
+    $endDate = Get-Date -Year $arrayEndDate[2] -Month $arrayEndDate[0] -Day $arrayEndDate[1]
+    $newEndDate = [DateTime]::ParseExact($endDate, "MM/dd/yyyy HH:mm:ss", $null)
+   
+    
+    $currentDate = $newStartDate
+
+    # section
+    $sectionId = $sectionId.ToString()
+    $query = "SELECT * FROM class WHERE section = '$sectionId';"
+    $connection.Open()
+    $command = New-Object MySql.Data.MySqlClient.MySqlCommand($query, $connection)
+    $adapter = New-Object MySql.Data.MySqlClient.MySqlDataAdapter($command)
+    $dataset = New-Object System.Data.DataSet
+    $adapter.Fill($dataset) | Out-Null
+    $table = $dataset.Tables[0]
+    $connection.Close()
+
+    while ($currentDate -le $newEndDate) {
+        # Perform your desired actions within the loop
+        # Write-Host $currentDate.ToString("yyyy-MM-dd")
+        # return $currentDate
+        $dayOfWeek = $currentDate.ToString("dddd", [System.Globalization.CultureInfo]::InvariantCulture)
+        $shortDay = $dayOfWeek.Substring(0, 2)
+        $shortDay = $shortDay.ToUpper()
+        
+        foreach ($date_db in $table.Rows) {
+        
+            if ($shortDay -eq $date_db['week_of_day']) {
+
+                $time = $date_db['start_time']
+                $date = Get-Date -Hour 8 -Minute 0 -Second 0 -Millisecond 0
+                $dateTime = $date.Date.Add([TimeSpan]::Parse($time))
+            
+                $durationtime = New-TimeSpan -Minutes $date_db['duration_time']
+                $newdatetime = $dateTime.Add($durationtime)
+
+                $curent_date = $currentDate.ToString("yyyy-MM-dd")
+                $start_time = $dateTime.ToString("H:mm")
+                $end_time = $newdatetime.ToString("H:mm")
+                Write-Host $curent_date $shortDay $start_time $end_time
+            }
+            # $shortDay = = $date_db['week_of_day']
+
+        }
+        # Write-Output $datofweek
+
+        # Increment the current date by one day
+        $currentDate = $currentDate.AddDays(1)
+
+    }
+}
+
 try {
     $connection = [MySql.Data.MySqlClient.MySqlConnection]@{ConnectionString = 'server=127.0.0.1;uid=root;pwd="";database=msteam' }
     $connection.Open()
@@ -117,7 +190,9 @@ foreach ($row in $table.Rows) {
             # Call function for add student
             # AddStudents -sectionId $section -groupId $groupId
             # AddInstructor -sectionId $section -groupId $groupId
-
+            # $startDate = $row['course_start_date']
+            # $endDate = $row['course_end_date']
+            # AddEvent -sectionId $section -groupId $groupId -startDate $startDate -endDate $endDate
         }
         else {
             Write-Output ("Crearte Fail {0}: {1}" -f $team, $row)
